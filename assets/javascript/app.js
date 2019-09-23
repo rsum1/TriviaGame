1b82ec9fed120cb5000f3f9aa745c01f0c068c6e
rsum1 <ryanjsumpter+github@gmail.com>
2019-09-22 20:00:52 -0700

    program renders questions on start button press

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+let triviaQuestions = [
+  {question: 'the answer is b',
+  choiceA: 'this is choice a',
+  choiceB: 'this is choice b',
+  choiceC: 'this is choice c',
+  choiceD: 'this is choice d',
+  answer: 'd'
+}
+]
