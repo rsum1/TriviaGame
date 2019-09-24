3e72931992842228f612b2348b93728c92e21d63
rsum1 <ryanjsumpter+github@gmail.com>
2019-09-23 18:09:39 -0700

    add done page, start check answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,26 @@
 let triviaQuestions = [
-  {question: 'the answer is b',
-  choiceA: 'this is choice a',
-  choiceB: 'this is choice b',
-  choiceC: 'this is choice c',
-  choiceD: 'this is choice d',
-  answer: 'd'
-}
+  {
+    question: 'the answer is c',
+    choiceA: 'this is choice a',
+    choiceB: 'this is choice b',
+    choiceC: 'this is choice c',
+    choiceD: 'this is choice d',
+    answer: 'c'
+  },
+  {
+    question: 'the answer is d',
+    choiceA: 'this is choice a',
+    choiceB: 'this is choice b',
+    choiceC: 'this is choice c',
+    choiceD: 'this is choice d',
+    answer: 'd'
+  },
+  {
+    question: 'the answer is b',
+    choiceA: 'this is choice a',
+    choiceB: 'this is choice b',
+    choiceC: 'this is choice c',
+    choiceD: 'this is choice d',
+    answer: 'b'
+  }
 ]
